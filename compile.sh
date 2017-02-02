33ca05b9e974d088d6d7b508e6a409f7e468d3c2
mzouist <mzou@mings-mbp.myhome.westell.com>
2017-02-02 00:07:08 -1000

    dynamic path for compile.sh

--- compile.sh
+++ compile.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+read -p "Enter Path to Derby Apache directory: "  ApachePath
+read -p "Enter Path to Java Home directory: "  JavaHome
 #STEP 1
-cd /Users/mzou/Documents/UHM/derby/Apache
-export DERBY_INSTALL=/Users/mzou/Documents/UHM/derby/Apache/db-derby-10.13.1.1-bin
+#/Users/mzou/Documents/UHM/derby/Apache
+cd $ApachePath
+export DERBY_INSTALL=$ApachePath/db-derby-10.13.1.1-bin
 export CLASSPATH=$DERBY_INSTALL/lib/derby.jar:$DERBY_INSTALL/lib/derbytools.jar:$DERBY_INSTALL/lib/derbynet.jar:$DERBY_INSTALL/lib/derbyclient.jar:.
-export DERBY_HOME=/Users/mzou/Documents/UHM/derby/Apache
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_111.jdk/Contents/Home/
+export DERBY_HOME=$ApachePath
+export JAVA_HOME=$JavaHome
+#/Library/Java/JavaVirtualMachines/jdk1.8.0_111.jdk/Contents/Home/
 
 cd $DERBY_INSTALL/bin
 
